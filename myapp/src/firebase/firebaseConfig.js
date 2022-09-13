8626f616eb9ac60725363300e5b5619ff7d000ad
Anna <kuzanna565@mail.ru>
2022-09-13 16:53:37 +0300

    new add

---
+++ myapp/src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAoDmgOLMN3Lk0w8MV_tLbZWUAhQxbNi_c",
+    authDomain: "homework9-16242.firebaseapp.com",
+    databaseURL: "https://homework9-16242-default-rtdb.firebaseio.com",
+    projectId: "homework9-16242",
+    storageBucket: "homework9-16242.appspot.com",
+    messagingSenderId: "608314249778",
+    appId: "1:608314249778:web:b2df0c5556e210604e222b",
+    measurementId: "G-GBE7382M0S"
+  };
---
+++ myapp/src/pages/Chats.jsx
@@ -0,0 +1,19 @@
+import { ChatList } from "../components/ChatList";
+import { Chat } from "../components/Chat";
+import { useParams } from "react-router-dom";
+import { Header } from "../components/Header";
+
+export const Chats = () => {
+    const { chatId } = useParams();
+    return (
+        <>
+        <Header />
+        <div className="App">
+          <ChatList />
+          <div className="formMessage">
+            <Chat chatId={chatId} />
+          </div>
+        </div>
+        </>
+  );
+};
